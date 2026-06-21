5213c094e4c0bad8688f1859178897b7b86fadc4
ost1 <ostl@outlook.com>
2026-06-21 23:28:01 +0800

    test

--- src/navigation.ts
+++ src/navigation.ts
@@ -6,7 +6,7 @@ export const headerData = {
       text: 'Homes',
       links: [
         {
-          text: 'SaaS',
+          text: 'SaaaaaaS',
           href: getPermalink('/homes/saas'),
         },
         {
